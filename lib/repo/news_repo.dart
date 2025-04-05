e69dab66f5edd659f7a4a95e6c0d66162e50db1c
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-05 18:39:03 +0200

    create news repo

--- lib/repo/news_repo.dart
+++ lib/repo/news_repo.dart
@@ -1,6 +1,10 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:news/Core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:news/Core/errors/failure.dart';
+import 'package:news/Models/articles_model.dart';
 
-// abstract class NewsRepo {
-//   Future<Either<Failure,>>
-// }
+abstract class NewsRepo {
+  Future<Either<Failure,ArticlesModel>> getBusiness();
+  Future<Either<Failure,ArticlesModel>> getSports();
+  Future<Either<Failure,ArticlesModel>> getSciences();
+  Future<Either<Failure,ArticlesModel>> getHealths();
+}
