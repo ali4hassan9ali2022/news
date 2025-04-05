bc1db85590c3bf426c8f52a02d532c47cccffd94
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-05 16:23:20 +0200

    create views to my app

---
+++ lib/Views/sport_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SportView extends StatelessWidget {
+  const SportView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
