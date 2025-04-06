25d1c6ae54c8a09fdde27030df748fd9cbbd68da
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-07 01:38:49 +0200

    create cache helper file

--- lib/Views/search_view.dart
+++ lib/Views/search_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news/Core/Widgets/category_item.dart';
 import 'package:news/Core/Widgets/category_list_view.dart';
-import 'package:news/Core/Widgets/custom_divider.dart';
 import 'package:news/Core/Widgets/custom_error_widget.dart';
 import 'package:news/Core/Widgets/custom_text_form_field.dart';
 import 'package:news/Cubit/Search_cubit/search_cubit.dart';
